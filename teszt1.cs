6df4cc6bb0e793643aa9a18566ed83fe12aa6efc
3101TAN-07 <3101TAN-07@3101-7>
2018-03-21 18:39:25 +0100

    asd

--- teszt1.cs
+++ teszt1.cs
@@ -4,8 +4,10 @@ public class Teszt1
 {
 	public Teszt1()
 	{
-       //első sor nada
-       // még 1 sor nb
-       //még1 bence
-	}
+        //első sor nada
+        // még 1 sor nb
+        //még1 bence
+
+        // ez már a Java?
+    }
 }
